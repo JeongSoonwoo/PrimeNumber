98c0ddae12a5214b7768f54fbd6a3c063bf7fed9
정순우 <soonwoo2714@gmail.com>
2018-03-29 16:13:24 +0900

    New

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -8,13 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var lblR: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var txt: UITextField!
+    @IBAction func btnC(_ sender: Any) {
+        var num = Int(txt.text!)
+        var pri = true
+        
+        if num == 1 {
+            pri = false
+        }
+        
+        if num != 2 && num != 1 {
+            for i in 2 ..< num! {
+                //print(i)
+                if num! % i == 0{
+                    pri = false
+                }
+            }
+        }
+        
+        if pri == true {
+            lblR.text = ("소수")
+        } else {
+            lblR.text = ("소수아님")
+        }
+        
 
+    }
+    @IBAction func btnR(_ sender: Any) {
+        txt.text = ""
+        lblR.text = ""
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
